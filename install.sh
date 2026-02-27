8594504aa1096d22b00da30998d94d7bb91120b9
Martin Munstermann <martin@munstermann.io>
2026-02-27 09:53:54 +0000

    Add tmux.conf setup to install.sh (copy example and symlink if not exists)

--- install.sh
+++ install.sh
@@ -16,3 +16,12 @@ sourceSh() {
 
 sourceSh '.zshrc'
 sourceSh '.bashrc'
+
+# Handle tmux.conf (copy example if not exists, then symlink)
+if [ ! -f ~/.tmux.conf ]; then
+  if [ -f ~/.my_sh/.tmux.conf.example ]; then
+    cp ~/.my_sh/.tmux.conf.example ~/.my_sh/.tmux.conf
+    ln -s ~/.my_sh/.tmux.conf ~/.tmux.conf
+    echo "Created ~/.tmux.conf -> ~/.my_sh/.tmux.conf"
+  fi
+fi
